74a7af953bb98a9e9ab68bcfb1116585ae379eca
Saurav Roy <srsaurav0@gmail.com>
2025-01-08 13:20:50 +0600

    Structure definition moved

--- controllers/property_details_controller.go
+++ controllers/property_details_controller.go
@@ -12,65 +12,6 @@ type PropertyDetailsController struct {
 	web.Controller
 }
 
-// Define the structure for the desired response
-type PropertyDetailsResponse struct {
-	ID        string `json:"ID"`
-	Feed      int    `json:"Feed"`
-	Published bool   `json:"Published"`
-	GeoInfo   struct {
-		Categories []struct {
-			Name       string   `json:"Name"`
-			Slug       string   `json:"Slug"`
-			Type       string   `json:"Type"`
-			Display    []string `json:"Display"`
-			LocationID string   `json:"LocationID"`
-		} `json:"Categories"`
-		City        string `json:"City"`
-		Country     string `json:"Country"`
-		CountryCode string `json:"CountryCode"`
-		Display     string `json:"Display"`
-		LocationID  string `json:"LocationID"`
-		StateAbbr   string `json:"StateAbbr"`
-		Lat         string `json:"Lat"`
-		Lng         string `json:"Lng"`
-	} `json:"GeoInfo"`
-	Property struct {
-		Amenities map[string]string `json:"Amenities"`
-		Counts    struct {
-			Bedroom   int `json:"Bedroom"`
-			Bathroom  int `json:"Bathroom"`
-			Reviews   int `json:"Reviews"`
-			Occupancy int `json:"Occupancy"`
-		} `json:"Counts"`
-		EcoFriendly  bool   `json:"EcoFriendly"`
-		FeatureImage string `json:"FeatureImage"`
-		Image        struct {
-			Count  int      `json:"Count"`
-			Images []string `json:"Images"`
-		} `json:"Image"`
-		Price                  int                `json:"Price"`
-		PropertyName           string             `json:"PropertyName"`
-		PropertySlug           string             `json:"PropertySlug"`
-		PropertyType           string             `json:"PropertyType"`
-		PropertyTypeCategoryId string             `json:"PropertyTypeCategoryId"`
-		ReviewScore            int                `json:"ReviewScore"`
-		ReviewScores           map[string]float64 `json:"ReviewScores"`
-		RoomSize               float64            `json:"RoomSize"`
-		MinStay                int                `json:"MinStay"`
-		UpdatedAt              string             `json:"UpdatedAt"`
-	} `json:"Property"`
-	Partner struct {
-		ID         string   `json:"ID"`
-		Archived   []string `json:"Archived"`
-		OwnerID    string   `json:"OwnerID"`
-		HcomID     string   `json:"HcomID"`
-		BrandId    string   `json:"BrandId"`
-		URL        string   `json:"URL"`
-		UnitNumber string   `json:"UnitNumber"`
-		EpCluster  string   `json:"EpCluster"`
-	} `json:"Partner"`
-}
-
 func (c *PropertyDetailsController) GetPropertyDetails() {
 	propertyId := c.Ctx.Input.Param(":propertyId")
 
@@ -81,7 +22,6 @@ func (c *PropertyDetailsController) GetPropertyDetails() {
 		c.Ctx.Output.SetStatus(http.StatusInternalServerError)
 		if writeErr := c.Ctx.Output.Body([]byte("Failed to fetch property details")); writeErr != nil {
 			c.Ctx.Output.SetStatus(http.StatusInternalServerError)
-			log.Fatalf("Failed to write 500 status of fetch property details")
 			// Log the error if you have a logging mechanism
 		}
 		return
@@ -93,7 +33,7 @@ func (c *PropertyDetailsController) GetPropertyDetails() {
 		c.Ctx.Output.SetStatus(http.StatusInternalServerError)
 		if writeErr := c.Ctx.Output.Body([]byte("Failed to decode response")); writeErr != nil {
 			c.Ctx.Output.SetStatus(http.StatusInternalServerError)
-			log.Fatalf("Failed to write 500 status of decode response")
+			log.Fatalf("Failed to write 500 status of ")
 		}
 		return
 	}
@@ -194,7 +134,7 @@ func (c *PropertyDetailsController) GetPropertyDetails() {
 		c.Ctx.Output.SetStatus(http.StatusInternalServerError)
 		if writeErr := c.Ctx.Output.Body([]byte("Failed to serve JSON response")); writeErr != nil {
 			c.Ctx.Output.SetStatus(http.StatusInternalServerError)
-			log.Fatalf("Failed to write 500 status of serve JSON response")
+			log.Fatalf("Failed to serve JSON response")
 		}
 	}
 }
