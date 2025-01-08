ce7110195bf771e17d63b8849edcea09ded077ad
Saurav Roy <srsaurav0@gmail.com>
2025-01-08 13:20:04 +0600

    Reusable structure added

--- controllers/default.go
+++ controllers/default.go
@@ -4,6 +4,64 @@ import (
 	beego "github.com/beego/beego/v2/server/web"
 )
 
+type PropertyDetailsResponse struct {
+	ID        string `json:"ID"`
+	Feed      int    `json:"Feed"`
+	Published bool   `json:"Published"`
+	GeoInfo   struct {
+		Categories []struct {
+			Name       string   `json:"Name"`
+			Slug       string   `json:"Slug"`
+			Type       string   `json:"Type"`
+			Display    []string `json:"Display"`
+			LocationID string   `json:"LocationID"`
+		} `json:"Categories"`
+		City        string `json:"City"`
+		Country     string `json:"Country"`
+		CountryCode string `json:"CountryCode"`
+		Display     string `json:"Display"`
+		LocationID  string `json:"LocationID"`
+		StateAbbr   string `json:"StateAbbr"`
+		Lat         string `json:"Lat"`
+		Lng         string `json:"Lng"`
+	} `json:"GeoInfo"`
+	Property struct {
+		Amenities map[string]string `json:"Amenities"`
+		Counts    struct {
+			Bedroom   int `json:"Bedroom"`
+			Bathroom  int `json:"Bathroom"`
+			Reviews   int `json:"Reviews"`
+			Occupancy int `json:"Occupancy"`
+		} `json:"Counts"`
+		EcoFriendly  bool   `json:"EcoFriendly"`
+		FeatureImage string `json:"FeatureImage"`
+		Image        struct {
+			Count  int      `json:"Count"`
+			Images []string `json:"Images"`
+		} `json:"Image"`
+		Price                  int                `json:"Price"`
+		PropertyName           string             `json:"PropertyName"`
+		PropertySlug           string             `json:"PropertySlug"`
+		PropertyType           string             `json:"PropertyType"`
+		PropertyTypeCategoryId string             `json:"PropertyTypeCategoryId"`
+		ReviewScore            int                `json:"ReviewScore"`
+		ReviewScores           map[string]float64 `json:"ReviewScores"`
+		RoomSize               float64            `json:"RoomSize"`
+		MinStay                int                `json:"MinStay"`
+		UpdatedAt              string             `json:"UpdatedAt"`
+	} `json:"Property"`
+	Partner struct {
+		ID         string   `json:"ID"`
+		Archived   []string `json:"Archived"`
+		OwnerID    string   `json:"OwnerID"`
+		HcomID     string   `json:"HcomID"`
+		BrandId    string   `json:"BrandId"`
+		URL        string   `json:"URL"`
+		UnitNumber string   `json:"UnitNumber"`
+		EpCluster  string   `json:"EpCluster"`
+	} `json:"Partner"`
+}
+
 type MainController struct {
 	beego.Controller
 }
