1c05a045ad37a4875ab28af61d5f115fc1ee359f
Saurav Roy <srsaurav0@gmail.com>
2025-01-10 19:04:31 +0600

    Swagger implemented

---
+++ swagger/docs.go
@@ -0,0 +1,12 @@
+package swagger
+
+// @APIVersion 1.0.0
+// @Title User Management API
+// @Description API for managing users
+// @Contact support@example.com
+// @TermsOfServiceUrl http://beego.me/
+// @License Apache 2.0
+// @LicenseUrl http://www.apache.org/licenses/LICENSE-2.0.html
+// @BasePath /api/v1
+func init() {
+}
