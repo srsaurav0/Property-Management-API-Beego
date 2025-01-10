8d4d561cfdf9773d2cc1e98219767dfddc4ee659
Saurav Roy <srsaurav0@gmail.com>
2025-01-10 12:32:05 +0600

    Database initialization added

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"beego-api-service/database"
 	_ "beego-api-service/routers"
+
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
+	database.Init()
 	beego.Run()
 }
-
