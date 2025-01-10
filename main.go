27dfbfae7888154b491990efee9763c67d378f85
Saurav Roy <srsaurav0@gmail.com>
2025-01-10 18:52:16 +0600

    Swagger configuration added

--- main.go
+++ main.go
@@ -4,10 +4,16 @@ import (
 	"beego-api-service/database"
 	_ "beego-api-service/routers"
 
-	beego "github.com/beego/beego/v2/server/web"
+	"github.com/beego/beego"
+	"github.com/beego/beego/v2/server/web"
+	_ "github.com/beego/beego/v2/server/web/swagger"
 )
 
 func main() {
 	database.Init()
+	if web.BConfig.RunMode == "dev" {
+		web.BConfig.WebConfig.DirectoryIndex = true
+		web.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
 	beego.Run()
 }
